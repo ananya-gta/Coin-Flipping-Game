bb6b47078c4ae73b7cab2119bc2b8c60f14fa75f
Ananya Gupta <anannya.gta@gmail.com>
2022-10-17 23:57:03 +0530

    Create CoinFlippingGame.java

---
+++ CoinFlippingGame.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class CoinFlippingGame {
+
+	public static void main(String[] args){
+		
+		// Create a coin game with the 2 players provided
+		
+		CoinGame theCoinGame = new CoinGame("Mark", "Tom");
+		
+		String usersAnswer;
+		
+		do {
+			
+			theCoinGame.startGame();
+			
+			System.out.println("Play Again? ");
+			
+			Scanner playGameAgain = new Scanner(System.in);
+			
+			usersAnswer = playGameAgain.nextLine();
+			
+		} while ((usersAnswer.startsWith("y")) || (usersAnswer.startsWith("Y")));
+		
+	}
+	
+}
